5881cb258e598eecbb74b0af5f4064d21735cdde
Tanya <kolt.levry@gmail.com>
2025-03-20 08:55:53 +0100

    Be 4. Create entities

--- src/main/java/de/dental_clinic/g_43_praxis/domain/entity/Appointment.java
+++ src/main/java/de/dental_clinic/g_43_praxis/domain/entity/Appointment.java
@@ -1,4 +1,162 @@
 package de.dental_clinic.g_43_praxis.domain.entity;
 
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "appointment")
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "phone2")
+    private String phone2;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "available_time", nullable = false)
+    private String availableTime;
+
+    private String comment;
+
+    @Column(nullable = false)
+    private String language;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Appointment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvailableTime() {
+        return availableTime;
+    }
+
+    public void setAvailableTime(String availableTime) {
+        this.availableTime = availableTime;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Appointment that = (Appointment) o;
+        return Objects.equals(id, that.id) && Objects.equals(service, that.service) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(phone, that.phone) && Objects.equals(phone2, that.phone2) && Objects.equals(email, that.email) && Objects.equals(availableTime, that.availableTime) && Objects.equals(comment, that.comment) && Objects.equals(language, that.language) && Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, service, firstName, lastName, phone, phone2, email, availableTime, comment, language, status);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Appointment: id = %d, serviceId = %d, firstName = %s, lastName = %s, phone = %s, email = %s, availableTime = %s, language = '%s', status = '%s'",
+                id,
+                service != null ? service.getId() : null,
+                firstName,
+                lastName,
+                phone,
+                email,
+                availableTime,
+                language,
+                status
+        );
+    }
 }
--- src/main/java/de/dental_clinic/g_43_praxis/domain/entity/Doctor.java
+++ src/main/java/de/dental_clinic/g_43_praxis/domain/entity/Doctor.java
@@ -1,4 +1,170 @@
 package de.dental_clinic.g_43_praxis.domain.entity;
+import jakarta.persistence.*;
 
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "doctor")
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titleDe;
+    private String titleEn;
+    private String titleRu;
+    private String fullName;
+    private String biographyDe;
+    private String biographyEn;
+    private String biographyRu;
+    private String specialisationDe;
+    private String specialisationEn;
+    private String specialisationRu;
+    private boolean isActive;
+    private String topImage;
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+    public Doctor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitleDe() {
+        return titleDe;
+    }
+
+    public void setTitleDe(String titleDe) {
+        this.titleDe = titleDe;
+    }
+
+    public String getTitleEn() {
+        return titleEn;
+    }
+
+    public void setTitleEn(String titleEn) {
+        this.titleEn = titleEn;
+    }
+
+    public String getTitleRu() {
+        return titleRu;
+    }
+
+    public void setTitleRu(String titleRu) {
+        this.titleRu = titleRu;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getBiographyDe() {
+        return biographyDe;
+    }
+
+    public void setBiographyDe(String biographyDe) {
+        this.biographyDe = biographyDe;
+    }
+
+    public String getBiographyEn() {
+        return biographyEn;
+    }
+
+    public void setBiographyEn(String biographyEn) {
+        this.biographyEn = biographyEn;
+    }
+
+    public String getBiographyRu() {
+        return biographyRu;
+    }
+
+    public void setBiographyRu(String biographyRu) {
+        this.biographyRu = biographyRu;
+    }
+
+    public String getSpecialisationDe() {
+        return specialisationDe;
+    }
+
+    public void setSpecialisationDe(String specialisationDe) {
+        this.specialisationDe = specialisationDe;
+    }
+
+    public String getSpecialisationEn() {
+        return specialisationEn;
+    }
+
+    public void setSpecialisationEn(String specialisationEn) {
+        this.specialisationEn = specialisationEn;
+    }
+
+    public String getSpecialisationRu() {
+        return specialisationRu;
+    }
+
+    public void setSpecialisationRu(String specialisationRu) {
+        this.specialisationRu = specialisationRu;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getTopImage() {
+        return topImage;
+    }
+
+    public void setTopImage(String topImage) {
+        this.topImage = topImage;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return isActive == doctor.isActive && Objects.equals(id, doctor.id) && Objects.equals(titleDe, doctor.titleDe) && Objects.equals(titleEn, doctor.titleEn) && Objects.equals(titleRu, doctor.titleRu) && Objects.equals(fullName, doctor.fullName) && Objects.equals(biographyDe, doctor.biographyDe) && Objects.equals(biographyEn, doctor.biographyEn) && Objects.equals(biographyRu, doctor.biographyRu) && Objects.equals(specialisationDe, doctor.specialisationDe) && Objects.equals(specialisationEn, doctor.specialisationEn) && Objects.equals(specialisationRu, doctor.specialisationRu) && Objects.equals(topImage, doctor.topImage) && Objects.equals(images, doctor.images);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titleDe, titleEn, titleRu, fullName, biographyDe, biographyEn, biographyRu, specialisationDe, specialisationEn, specialisationRu, isActive, topImage, images);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Doctor: id = %d, fullName = %s, title = %s, specialisation = %s, isActive = %b, topImage = '%s' ",
+                id,
+                fullName,
+                titleEn,
+                specialisationEn,
+                isActive,
+                topImage
+        );
+    }
+
 }
