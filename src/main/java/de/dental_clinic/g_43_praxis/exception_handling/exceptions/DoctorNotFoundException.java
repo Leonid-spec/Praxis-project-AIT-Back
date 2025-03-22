bd51ac025fdfbeee7afa8e5511f363935a6181e5
IgorSv <igorexa08@gmail.com>
2025-03-22 13:33:32 +0100

    doctor_service_controller

--- src/main/java/de/dental_clinic/g_43_praxis/exception_handling/exceptions/DoctorNotFoundException.java
+++ src/main/java/de/dental_clinic/g_43_praxis/exception_handling/exceptions/DoctorNotFoundException.java
@@ -1,4 +1,7 @@
 package de.dental_clinic.g_43_praxis.exception_handling.exceptions;
 
-public class DoctorNotFoundException {
+public class DoctorNotFoundException extends RuntimeException {
+    public DoctorNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/de/dental_clinic/g_43_praxis/repository/DoctorRepository.java
+++ src/main/java/de/dental_clinic/g_43_praxis/repository/DoctorRepository.java
@@ -5,14 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
     List<Doctor> findAllByIsActiveTrue();
-
-    Optional<Doctor> findByFullName(String fullName);
 
     boolean existsByFullName(String fullName);
 }
