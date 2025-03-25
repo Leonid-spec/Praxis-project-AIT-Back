d97798fb6e27b3c0281b6342a4ede7daa181ca64
Tanya Maxar <kolt.levry@gmail.com>
2025-03-25 10:33:24 +0100

    check version

--- src/main/java/de/dental_clinic/g_43_praxis/controller/AdminController.java
+++ src/main/java/de/dental_clinic/g_43_praxis/controller/AdminController.java
@@ -1,4 +1,32 @@
 package de.dental_clinic.g_43_praxis.controller;
 
+import de.dental_clinic.g_43_praxis.domain.dto.AdminDto;
+import de.dental_clinic.g_43_praxis.domain.entity.Admin;
+import de.dental_clinic.g_43_praxis.exception_handling.Response;
+import de.dental_clinic.g_43_praxis.repository.AdminRepository;
+import de.dental_clinic.g_43_praxis.service.AdminServiceImpl;
+import de.dental_clinic.g_43_praxis.service.mapping.AdminMappingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+
 public class AdminController {
+
+    private final AdminServiceImpl adminService;
+    private final AdminRepository adminRepository;
+
+    public AdminController(AdminServiceImpl adminService, AdminRepository adminRepository) {
+        this.adminService = adminService;
+        this.adminRepository = adminRepository;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> checkAdmin(@RequestBody AdminDto adminDto) {
+        adminService.login(adminDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
